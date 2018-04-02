4584dce29fe7745ebc5ee48651278a661575493d
ModelFox <38005108+ModelFox@users.noreply.github.com>
2018-04-02 14:58:04 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,51 +1,9 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const fs = require("fs");
-   
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
+const client = new Discord.Client();
+
+client.on('ready', function() => {
+  client.user.setGame('Reset System, h!help')
+  console.log('Logged in as ${client.user.tag}!');
 });
-  
-bot.on('message', message => {
-  bot.on('ready', () => {
-  bot.user.setGame('Reset System, h!help')
-  
-var userData = JSON.parse(fs.readFileSync('jsonPointID/level.json', 'utf-8'));
-var sender = message.author;
-var msg = message.content.toUpperCase();
-var prefix = 'h!'
-  
-  
-//if (msg === prefix + 'MESSAGESTATS') {
-  //  message.channel.send('Vous avez envoyé **' + userData[sender.id].messagesSent + '** messages !' )
-//}
- 
-if (msg === prefix + 'help') {
-    message.channel.send('la commande ">messagestats" sert a voir le nombre de message que vous avez envoyé au total sur ce serveur.')
-}
-  
-if (!userData[sender.id]) userData[sender.id] = {
-    messagesSent: 0
-}
- 
- 
- 
-userData[sender.id].messagesSent++;
- 
-if (msg === prefix + 'MESSAGESTATS') {
- 
-message.channel.send({embed: {
-    title: "Total de messages envoyé",
-    description: "Messages envoyé au total : " + userData[sender.id].messagesSent ,
-    color: "3447003"
-}})
- 
-}
- 
-fs.writeFile('jsonPointID/level.json', JSON.stringify(userData), (err) => {
-    if (err) console.error(err);
-});
-  
-});
- 
-    bot.login(process.env.TOKEN);
+
+bot.login(process.env.TOKEN)
